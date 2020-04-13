bc74bc9f9c8d1b108b354549d446a658cbfac66b
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-13 15:43:08 +0530

    added the methods

--- lru.py
+++ lru.py
@@ -1,4 +1,26 @@
+from datetime import datetime
+
 class LRUItem(obj):
+    #items in cache.
     def __init__(self,key,data):
         self.key = key
         self.data = data
+        self.timestamp = datetime.now()
+
+class LRUCache(obj):
+    def __init__(self,len,delta = None):
+        self.len = len
+        self.delta = delta
+        self.hash = {}
+        self.data_list = []
+
+    def insert(self,data):
+        pass
+
+
+    def delete(self,data):
+        pass
+
+
+    def validate(self):
+        pass
