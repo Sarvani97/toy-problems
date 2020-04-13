fa4a2005e12afa8749d7fbe9cff52a56ad8d670b
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-13 17:28:43 +0530

    completed methods

--- lru.py
+++ lru.py
@@ -8,19 +8,42 @@ class LRUItem(obj):
         self.timestamp = datetime.now()
 
 class LRUCache(obj):
-    def __init__(self,len,delta = None):
+    def __init__(self,len,time = None):
         self.len = len
-        self.delta = delta
+        self.time = time
         self.hash = {}
         self.data_list = []
 
     def insert(self,data):
-        pass
+        if data.key in self.hash:
+            #move the visited data(if in the list) in the list to the top.
+            data_idx = self.data_list.index(data)
+            self.data_list[:] = self.data_list[:data_idx] + self.data_list[data_idx + 1:]
+            self.data_list.insert(0,data)
+
+        else:
+            #remove the last item if the size exceeds
+            if len(self.data_list) > self.len:
+                self.remove(self.data_list[-1])
+            #append to the front of the list
+            self.hash[data.key] = data
+            self.data_list.insert(0,data)
+
 
 
     def delete(self,data):
-        pass
+        del self.hash[data.key]
+        del self.data_list[self.data_list.index(data)]
+
 
 
     def validate(self):
-        pass
+        def oldData():
+            now = datetime.now()
+            for d in data_list:
+                timeLeft = now - data.timestamp
+                if timeLeft.seconds > self.time:
+                    yield data
+        map(lambda x: self.remove(data), oldData())
+    
+
