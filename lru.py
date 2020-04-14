7ceac805dc2216f9d9f62b368eb2bc457a400ec4
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-14 12:30:19 +0530

    completed the logic

--- lru.py
+++ lru.py
@@ -1,25 +1,30 @@
-from datetime import datetime
-
 class LRUItem(object):
     #items in cache.
-    def __init__(self,key,data):
+    def __init__(self,key,val):
         self.key = key
-        self.data = data
-        self.timestamp = datetime.now()
+        self.val = val
 
 class LRUCache(object):
-    def __init__(self,len,time = None):
-        self.len = len
-        self.time = time
-        self.hash = {}
+    def __init__(self):
         self.data_list = []
 
     def put(self,data):
-        pass
+        for d in self.data_list:
+            if d[0] == data.key:
+                return "key already exists"
+        if(len(self.data_list) == 4):
+            self.data_list.pop(0)
+            self.data_list.append((data.key,data.val))
+        else:
+            self.data_list.append((data.key,data.val))
 
-    def get(self,data):
-        pass
+    def get(self,key):
+        for i in self.data_list:
+            if i[0] == key:
+                print("key already exists")
+            else:
+                print("key doesn't exist")
 
     def get_cache(self):
-        pass
+        return self.data_list
 
