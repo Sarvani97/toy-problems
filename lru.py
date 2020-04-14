e341b9ab2504aaf4726fc3ee56d95597f05ba8f1
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-14 11:33:01 +0530

    completed method sigatures

--- lru.py
+++ lru.py
@@ -14,38 +14,12 @@ class LRUCache(object):
         self.hash = {}
         self.data_list = []
 
-    def insert(self,data):
-        pass
-        #if data.key in self.hash:
-            #move the visited data(if in the list) in the list to the top.
-            #data_idx = self.data_list.index(data)
-            #self.data_list[:] = self.data_list[:data_idx] + self.data_list[data_idx + 1:]
-            #self.data_list.insert(0,data)
+def insert(self,data):
+    pass
 
-        #else:
-            #remove the last item if the size exceeds
-            #if len(self.data_list) > self.len:
-                #self.remove(self.data_list[-1])
-            #append to the front of the list
-            #self.hash[data.key] = data
-            #self.data_list.insert(0,data)
+def delete(self,data):
+    pass
 
+def validate(self):
+    pass
 
-
-    def delete(self,data):
-        pass
-        #del self.hash[data.key]
-        #del self.data_list[self.data_list.index(data)]
-
-
-
-    def validate(self):
-        pass
-        #def oldData():
-            #now = datetime.now()
-            #for d in data_list:
-                #timeLeft = now - data.timestamp
-                #if timeLeft.seconds > self.time:
-                    #yield data
-        #map(lambda x: self.remove(data), oldData())
-    
