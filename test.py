5c799adcda7464e907ba1999511e1af840ee7f22
sarvani97 <saisreesarvani@msitprogram.net>
2020-04-14 13:04:34 +0530

    test cases

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 from lru import *
 
-e1 = (1,'one')
-e2 = (2,'two')
-e3 = (3,'three')
-e4 = (4,'four')
-e5 = (5,'five')
-e6 = (6,'six')
-e7 = (7,'seven')
+e1 = LRUItem(1,'one')
+e2 = LRUItem(2,'two')
+e3 = LRUItem(3,'three')
+e4 = LRUItem(4,'four')
+e5 = LRUItem(5,'five')
+e6 = LRUItem(6,'six')
+e7 = LRUItem(7,'seven')
 
 class LruTest:
     cache = LRUCache()
     cache.put(e1)
 
     result = cache.get_cache()
-    assert result == [(1,'one')]
+    assert result == [(1, 'one')]
 
     cache.put(e2)
     cache.put(e3)
@@ -26,9 +26,9 @@ class LruTest:
 
     cache.put(e6)
     result == cache.get_cache()
-    assert result == [(1,'one'),(2,'two'),(3,'three'),(4,'four'),(5,'five'),(6,'six')]
+    assert result == [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'), (6, 'six')]
 
-    result = cache.put(e5)
+    result = cache.put(e4)
     assert result == "key already exists"
 
     print("All test cases passed")
